fd51754921dbc96c34fa711ae2c5d69348b473f4
Mathias <mathias.kirkerod@gmail.com>
2018-03-22 15:25:56 +0100

    added quick install

---
+++ install.sh
@@ -0,0 +1,9 @@
+pip install ./src/matkirpack/.
+pip install ./src/matkirpack/. --upgrade
+
+
+wget http://datasets.simula.no/kvasir/data/kvasir-dataset-v2.zip
+unzip -o kvasir-dataset-v2.zip
+rm kvasir-dataset-v2.zip
+
+
