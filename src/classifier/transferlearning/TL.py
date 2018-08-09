2fec8ec391ba24915e365115d91370402153560c
Mathias <mathias.kirkerod@gmail.com>
2018-08-09 12:20:42 +0200

    changes

--- src/classifier/transferlearning/TL.py
+++ src/classifier/transferlearning/TL.py
@@ -10,8 +10,8 @@ import os
 
 class TL():
     def __init__(self):
-        self.img_rows = 576//8
-        self.img_cols = 720//8        
+        self.img_rows = 576//2
+        self.img_cols = 720//2        
         self.channels = 3
         self.img_shape = (self.img_cols, self.img_rows, self.channels)
         model,l_out=self.make_model()
@@ -44,7 +44,7 @@ class TL():
                                                             batch_size = self.batch_size, 
                                                             class_mode = "categorical")
         
-        test_datagen = ImageDataGenerator(
+        val_datagen = ImageDataGenerator(
             rescale = 1./255,
             horizontal_flip = True,
             vertical_flip = True,
@@ -54,30 +54,40 @@ class TL():
             height_shift_range=0.3,
             rotation_range=30)
         
-        validation_generator = test_datagen.flow_from_directory(self.val_dir,
+        validation_generator = val_datagen.flow_from_directory(self.val_dir,
                                                                 target_size = (self.img_cols, self.img_rows),
                                                                 class_mode = "categorical")
         
         
+        test_datagen = ImageDataGenerator(rescale = 1./255)
+        test_generator = test_datagen.flow_from_directory(self.val_dir,
+                                                                target_size = (self.img_cols, self.img_rows),
+                                                                class_mode = "categorical")
+      
+       
         checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+        checkpoint = ModelCheckpoint("densenet_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
         early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+        board = TensorBoard()
         
         # Train the model 
         self.VGG.fit_generator(
             train_generator,
-            samples_per_epoch = 5000,
+            steps_per_epoch= 5000,
             epochs = 20,
-            #validation_data = validation_generator,
-            #nb_val_samples = nb_validation_samples,
-            callbacks = [checkpoint, early])
-        
+            validation_data = validation_generator,
+            nb_val_samples = 100,
+            callbacks = [checkpoint, early, board])
+        self.VGG.evaluate_generator(generator, 
+                                   verbose=1)
         
     def make_model(self):
         model = applications.VGG19(weights = "imagenet", include_top=False,input_shape = self.img_shape)
-        #model.summary()
+        model = applications.DenseNet201(include_top=False,input_shape=self.img_shape)     
+        model.summary()
         
         #Freezing 
-        for layer in model.layers[:15]:
+        for layer in model.layers[:700]:
             layer.trainable = False
         
         #adding custom layer
