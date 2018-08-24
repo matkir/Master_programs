49fc9c3b8167a7a4c14dc072bc8c46455827fc3b
Mathias <mathias.kirkerod@gmail.com>
2018-08-24 17:09:15 +0200

    half way in to contextencoder

--- src/gan/contextencoder/context_weights.py
+++ src/gan/contextencoder/context_weights.py
@@ -11,27 +11,26 @@ import keras.backend as K
 import numpy as np
 
 class Weight_model():
-    def __init__(self,img_rows,img_cols,mask_width,mask_height,corner=False):
-        self.img_rows = img_rows 
+    def __init__(self,img_rows,img_cols,corner=True):
         self.img_cols = img_cols 
-        self.mask_width = mask_width
-        self.mask_height = mask_height 
+        self.img_rows = img_rows 
         self.channels = 3
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        if corner:
+            dummy=plotload.load_one_img(self.img_shape, dest='none',extra_dim=True)
+            a, b, dims = ms.mask_green_corner(dummy)
+            self.mask_width = dims[3]-dims[2]
+            self.mask_height = dims[1]-dims[0]
+        else:        
+            self.mask_width = 62#208
+            self.mask_height = 51#280
+        
+        self.mask_width = mask_width
+        self.mask_height = mask_height 
         self.missing_shape = (self.mask_height, self.mask_width, self.channels)
-        self.corner=corner
    
     def build_generator_img_size(self):
-        """
-        def residual_block(x,channels_out, strides=(1, 1),):
-            #NOTE, not in use
-            shortcut=x
-            x1=x(Conv2D(channels_out, kernel_size=3, strides=strides, padding="same")))
-            x2=x1(LeakyReLU(alpha=0.2))
-            x3=x2(BatchNormalization(momentum=0.8))
-            x4 = layers.add([shortcut, x3])
-            return x4
-        """            
+         
         model = Sequential()
 
         # Encoder
@@ -85,7 +84,7 @@ class Weight_model():
             target_height=(height-self.mask_height)/2
             target_width=(width-self.mask_width)/2
             
-            #MIGHT BE WRONG
+            #MIGHT BE WRONG, but probably not, lol
             model.add(Cropping2D((
                 (int(np.ceil(target_height)),int(np.floor(target_height))),
                 (int(np.ceil(target_width)),int(np.floor(target_width))))))
@@ -103,65 +102,7 @@ class Weight_model():
         return Model(masked_img, gen_missing)
 
 
-    def build_generator(self):
 
-
-        model = Sequential()
-
-        # Encoder
-        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        
-        model.add(Dropout(0.5))
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        
-        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        
-        model.add(Conv2D(128, kernel_size=3, strides=3, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(Conv2D(512, kernel_size=1, strides=3, padding="same"))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.5))
-
-        # Decoder
-        model.add(UpSampling2D(size=(3,3)))
-        model.add(Conv2D(128, kernel_size=3, padding="same"))
-        model.add(Activation('relu'))
-        
-        model.add(UpSampling2D(size=(3,3)))
-        model.add(Conv2D(128, kernel_size=3, padding="same"))
-        model.add(Activation('relu'))
-        
-        model.add(UpSampling2D())
-        model.add(Conv2D(128, kernel_size=3, padding="same"))
-        model.add(Activation('relu'))
-       
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(UpSampling2D())
-        model.add(Dropout(0.5))
-        model.add(Conv2D(64, kernel_size=3, padding="same"))
-        model.add(Activation('relu'))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Conv2D(self.channels, kernel_size=3, padding="same"))
-        model.add(Activation('tanh'))
-
-        model.summary()
-
-        masked_img = Input(shape=self.img_shape)
-        gen_missing = model(masked_img)
-
-        return Model(masked_img, gen_missing)
 
     def build_discriminator(self):
 
@@ -198,22 +139,39 @@ class Weight_model():
 
         return Model(img, validity)
 
-    def sampling(self,args):
-        """Reparameterization trick by sampling fr an isotropic unit Gaussian.
-    
-        # Arguments:
-            args (tensor): mean and log of variance of Q(z|X)
-    
-        # Returns:
-            z (tensor): sampled latent vector
-        """
-    
-        z_mean, z_log_var = args
-        batch = K.shape(z_mean)[0]
-        dim = K.int_shape(z_mean)[1]
-        # by default, random_normal has mean=0 and std=1.0
-        epsilon = K.random_normal(shape=(batch, dim))
-        return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+    def contextencoder():
+        optimizer = Adam(0.0002, 0.5)
+        
+        #Build and compile the discriminator
+        self.discriminator = build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build and compile the generator
+        self.generator = build_generator_img_size()
+        #self.generator.compile(loss=['binary_crossentropy'],
+        #    optimizer=optimizer)
+
+        # The generator takes noise as input and generates the missing
+        # part of the image
+        masked_img = Input(shape=self.img_shape)
+        gen_missing = self.generator(masked_img)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines
+        # if it is generated or if it is a real image
+        valid = self.discriminator(gen_missing)
+
+        # The combined model  (stacked generator and discriminator) takes
+        # masked_img as input => generates missing image => determines validity
+        self.combined = Model(masked_img , [gen_missing, valid])
+        self.combined.compile(loss=['mse', 'binary_crossentropy'],
+            loss_weights=[0.7, 0.3],
+            optimizer=optimizer)        
 
 
 if __name__=='__main__':
---
+++ src/gan/contextencoder/contextencoder (copy).py
@@ -0,0 +1,295 @@
+from __future__ import print_function, division
+
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply, GaussianNoise
+from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D
+from keras.layers import MaxPooling2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+from keras import losses
+from keras.utils import to_categorical
+import keras.backend as K
+import sys
+from context_weights import Weight_model
+import masker as ms
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import plotload
+import sys
+import numpy as np
+
+class ContextEncoder():
+    def __init__(self):
+        """
+        Initializes the contextencoder. 
+        """
+        self.set_training_info()
+        globals().update(self.info)  
+        self.threshold=threshold
+        self.img_cols = 256 # Original is ~576
+        self.img_rows = 256 # Original is ~720 
+        self.channels = 3   # RGB 
+        self.img_shape=(self.img_cols,self.img_rows,self.channels)
+        self.model=None
+        self.pretrained=False
+    def load_model(self,adress):
+        """
+        loads a model to the object instead of creating one. 
+        :param adress: string of adress to the file of type h5.
+        """
+        if self.model!=None:
+            print("Warning: overriding a loaded model")
+        self.model=load_model(adress) 
+    def load_model_weights(self,adress):
+        """
+        loads weights to the model. 
+        :param model:  the model that is to get weights
+        :param adress: string of adress to the file of type h5.
+        :returns:      model with weights
+        """
+        if self.model==None:
+            print("Error: no model in object")
+        else:
+            try:
+                self.model.load_weights(adress)
+                self.pretrained=True
+            except e:
+                print("Error: weights could not be loaded")
+                print(e)
+    
+    def build_model(self):
+        """
+        builds a model to the object instead of loading one. 
+        Uses AE_weights.py as model
+        """
+        if self.model!=None:
+            print("Warning: overriding a loaded model")
+        wm=Weight_model(self.img_shape)
+        self.model=wm.build_AE()  
+        
+    def set_training_info(self):
+        self.info={}
+        import sys
+        try:
+            if len(sys.argv)==1:
+                choise=2
+            else:
+                choise=int(input("press 1 for last run or 2 for info.txt "))
+        except:
+            choise=False
+        
+        if choise==1:
+            self.info=np.load("temp_info.npy").item()
+            return
+        elif choise==2:
+            with open("info.txt") as f:
+                for line in f:
+                    (key, val) = line.split()
+                    try:
+                        self.info[key] = int(val)
+                    except:
+                        self.info[key] = float(val)
+            np.save("temp_info.npy", self.info)
+            return
+        else:        
+            self.info["mask"]=int(input("Mask [1] or corner [0]? "))
+            if self.info['mask']==1:
+                tmp=input("Mask adress? (default: /masks) ")
+                self.info["mask_folder"]=tmp if isinstance(tmp, str) else "/masks"
+            self.info["epochs"]=int(input("Number of epochs? "))
+            self.info["batch_size"]=int(input("Batch size? "))
+            self.info["save_interval"]=int(input("save interval? "))
+            np.save("temp_info.npy", self.info)    
+    
+    
+    
+    
+    
+    
+        
+    def extra(self):
+        if '-corner' in sys.argv:
+            dummy=plotload.load_one_img(self.img_shape, dest='none',extra_dim=True)
+            a, b, dims = ms.mask_green_corner(dummy)
+            self.mask_width = dims[3]-dims[2]
+            self.mask_height = dims[1]-dims[0]
+            corner=True
+        else:        
+            self.mask_width = 62#208
+            self.mask_height = 51#280
+            corner=False
+        
+        self.missing_shape = (self.mask_height, self.mask_width, self.channels)
+        self.model=Weight_model(self.img_cols, self.img_rows, self.mask_height,self.mask_width,corner)
+
+        optimizer = Adam(0.0002, 0.5)
+        
+        #Build and compile the discriminator
+        self.discriminator = self.model.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build and compile the generator
+        self.generator = self.model.build_generator_img_size()
+        self.generator.compile(loss=['binary_crossentropy'],
+            optimizer=optimizer)
+
+        if '-weights' in sys.argv:
+            print("loading old weights")
+            self.generator.load_weights("saved_model/generator_weigths.h5")
+            self.discriminator.load_weights("saved_model/discriminator_weigths.h5")
+        # The generator takes noise as input and generates the missing
+        # part of the image
+        masked_img = Input(shape=self.img_shape)
+        gen_missing = self.generator(masked_img)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines
+        # if it is generated or if it is a real image
+        valid = self.discriminator(gen_missing)
+
+        # The combined model  (stacked generator and discriminator) takes
+        # masked_img as input => generates missing image => determines validity
+        self.combined = Model(masked_img , [gen_missing, valid])
+        self.combined.compile(loss=['mse', 'binary_crossentropy'],
+            loss_weights=[0.7, 0.3],
+            optimizer=optimizer)
+        if "-save" in sys.argv:
+            self.generator.save("saved_model/generator.h5")
+            self.discriminator.save("saved_model/discriminator.h5")
+
+
+
+    def train(self, epochs, batch_size=128, sample_interval=50):
+        half_batch = int(batch_size / 2)
+
+        """
+        X_train=plotload.load_polyp_data(self.img_shape)
+        """
+        soft= True if '-soft' in sys.argv else False
+        corner= True if '-corner' in sys.argv else False
+        numtimes=np.zeros(batch_size)
+        from keras.callbacks import TensorBoard
+        board=TensorBoard()
+        board.set_model(self.discriminator)
+        for epoch in range(epochs):
+
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random half batch of images
+                
+            if epoch%100==0:
+                print(f"most used picture was traned on {max(numtimes)} times")
+                numtimes=np.zeros(batch_size)        
+                X_train=plotload.load_polyp_batch(self.img_shape, batch_size, data_type='none',crop=True)
+            if epoch%50==0 and not corner:
+                #after 50 itterations we flip the images, to make the set 2x times as large. sorry for not vectorizing
+                for i in range(batch_size):
+                    X_train[i]=np.fliplr(X_train[i])
+            idx = np.random.randint(0, X_train.shape[0], half_batch)
+            imgs = X_train[idx]
+
+            numtimes[idx]+=1 #to count num of times each pic was trained on
+            
+            if corner:    
+                masked_imgs, missing, _ = ms.mask_green_corner(imgs)
+            else:
+                masked_imgs, missing, _ = ms.mask_randomly_square(imgs, 
+                    self.mask_height, 
+                    self.mask_width)
+
+            # Generate a half batch of new images
+            gen_missing = self.generator.predict(masked_imgs)
+
+            if soft:
+                valid = 0.2*np.random.random_sample((half_batch,1))+0.9
+                fake = 0.1*np.random.random_sample((half_batch,1))
+            else:
+                valid = np.ones((half_batch, 1))
+                fake = np.zeros((half_batch, 1))
+
+            if epoch%120==0:
+                #small shakeup to get out of local minimas
+                placeholder=valid
+                valid=fake
+                fake=placeholder
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch(missing, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_missing, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+            # Select a random half batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
+    
+            if corner:
+                masked_imgs, missing_parts, _ = ms.mask_green_corner(imgs)
+            else:
+                masked_imgs, missing_parts, _ = ms.mask_randomly_square(imgs,self.mask_height,self.mask_width)
+                
+            # Generator wants the discriminator to label the generated images as valid
+            valid = np.ones((batch_size, 1))
+    
+            # Train the generator
+            g_loss = self.combined.train_on_batch(masked_imgs, [missing_parts, valid])
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc: %.2f%%] [G loss: %f, mse: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss[0], g_loss[1]))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                # Select a random half batch of images
+                imgs=plotload.load_polyp_batch(self.img_shape, 4, 
+                                         data_type='green', 
+                                         crop=True)
+                self.sample_images(epoch, imgs)
+            if epoch % (sample_interval*5) == 0:
+                self.save_model()   
+    def sample_images(self, epoch, imgs):
+        r, c = 3, 4
+
+        #masked_imgs, missing_parts, (y1, y2, x1, x2) = ms.mask_randomly_square(imgs,self.mask_height,self.mask_width)
+        masked_imgs, missing_parts, (y1, y2, x1, x2) =ms.mask_green_corner(imgs)   
+        gen_missing = self.generator.predict(masked_imgs)
+
+        imgs = 0.5 * imgs + 0.5
+        masked_imgs = 0.5 * masked_imgs + 0.5
+        gen_missing = 0.5 * gen_missing + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        for i in range(c):
+            axs[0,i].imshow(imgs[i, :,:])
+            axs[0,i].axis('off')
+            axs[1,i].imshow(masked_imgs[i, :,:])
+            axs[1,i].axis('off')
+            filled_in = imgs[i].copy()
+            filled_in[x1:x2, y1:y2, :] = gen_missing[i]
+            axs[2,i].imshow(filled_in)
+            axs[2,i].axis('off')
+        fig.savefig("images/cifar_%d.png" % epoch)
+        plt.close()
+
+    def save_model(self):
+        self.generator.save_weights("saved_model/generator_weigths.h5")
+        self.discriminator.save_weights("saved_model/discriminator_weigths.h5")
+
+
+if __name__ == '__main__':
+    context_encoder = ContextEncoder()
+    context_encoder.train(epochs=2000, batch_size=320, sample_interval=100)
+    imgs=plotload.load_polyp_batch((576//6,720//6,3), 4, data_type='green', crop=False)    
+    context_encoder.sample_images(2, imgs)
+    context_encoder.train(epochs=10000, batch_size=1024, sample_interval=100)
+
+
